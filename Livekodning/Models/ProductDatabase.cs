de4cb9bb788490d25bef216fdb9b179282cafa74
Kalle <kr@impera.se>
2019-10-18 11:45:35 +0200

    Resultat av livekodning 2019-10-18

---
+++ Livekodning/Models/ProductDatabase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Livekodning.Models {
+    public class ProductDatabase {
+        public List<Product> Products { get; private set; }
+
+        public delegate void ProductsAddedHandlerMethod(List<Product> products);
+        public event ProductsAddedHandlerMethod ProductsAdded;
+
+        public ProductDatabase() {
+            Products = new List<Product>();
+        }
+
+        public void AddProduct(Product product) {
+            Products.Add(product);
+            var newProducts = new List<Product>();
+            newProducts.Add(product);
+            FireProductsAddedEvent(newProducts);
+        }
+
+        public void AddProducts(List<Product> products) {
+            Products.AddRange(products);
+            FireProductsAddedEvent(products);
+        }
+
+        public List<Product> GetProductsByType(ProductType type) {
+            //var result = new List<Product>();
+            //foreach (var product in Products) {
+            //    if (product.Type == type) {
+            //        result.Add(product);
+            //    }
+            //}
+            //return result;
+
+            return Products
+                .Where((Product p) => p.Type == type)
+                .ToList();
+        }
+
+        private void FireProductsAddedEvent(List<Product> newProducts) {
+            if (ProductsAdded != null) {
+                ProductsAdded(newProducts);
+            }
+        }
+    }
+}
--- Livekodning/Program.cs
+++ Livekodning/Program.cs
@@ -5,26 +5,26 @@ using Livekodning.Services;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Livekodning {
     class Program {
+        static void HandleProductsAdded(List<Product> newProducts) {
+            foreach (var product in newProducts) {
+                Console.WriteLine("Added new Product: " + product.GetPrintableString());
+            }
+        }
         static void Main(string[] args) {
             var serializerService = new ProductSerializerService();
             var jsonFilename = "products.json";
+            var productDb = new ProductDatabase();
+            productDb.ProductsAdded += HandleProductsAdded;
             try {
                 if (File.Exists(jsonFilename)) {
                     var productList = serializerService.Deserialize(jsonFilename);
-                    foreach (var product in productList) {
-                        Console.WriteLine(product.GetPrintableString());
-                    }
-                } else {
-                    var productList = new List<Product> {
-                    new Song("Time and Season", 10),
-                    new Phone("Pixel", 1500)
-                };
-                    serializerService.Serialize(jsonFilename, productList);
-                    Console.WriteLine("Wrote products to " + jsonFilename);
+                    productDb.AddProducts(productList.ToList());
                 }
+
             } catch (ProductSerializerServiceException ex) {
                 Console.WriteLine($"Exception in file {ex.Filename}: {ex.Message}");
             } catch (Exception ex) {
